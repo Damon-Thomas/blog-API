b0fb551b52ace75fa59a5ff3b3a583dadcb88604
Damon Thomas <damon22t@gmail.com>
2025-01-17 17:28:31 -0500

    update dependancies

---
+++ backendAPI/controllers/userController.js
@@ -0,0 +1,9 @@
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+
+module.exports = {
+    
+}
