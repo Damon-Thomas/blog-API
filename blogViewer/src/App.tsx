346685bac10870b111ffdef90592d724d2035d23
Damon Thomas <damon22t@gmail.com>
2025-01-25 11:41:01 -0500

    delete old and change project structure

---
+++ blogViewer/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import HeaderBar from './components/HeaderBar'
+
+function App() {
+
+  return (
+    <div className="p-4 md:p-6 lg:p-8 mainBody flex flex-col content-start w-screen min-h-screen">
+      <HeaderBar />
+    </div>
+  )
+}
+
+export default App
