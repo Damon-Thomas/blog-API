1ac416ae121e7485165e68dbd882e93df7398d95
Damon Thomas <damon22t@gmail.com>
2025-02-05 09:54:51 -0500

    fix overflow issue

--- blogViewer/src/components/Header/HeaderBar.tsx
+++ blogViewer/src/components/Header/HeaderBar.tsx
@@ -5,7 +5,7 @@ import Logo from "./Logo";
 
 export default function HeaderBar() {
   return (
-    <div className="headerBar backdrop-blur-sm sticky top-0 h-16 bg-opaqueWhite flex px-4 rounded-md justify-between shadow-xl">
+    <div className="headerBar backdrop-blur-sm  sticky top-0 h-16 bg-opaqueWhite flex px-4 rounded-md justify-between shadow-xl">
       <Logo />
       <div className="headerRight flex justify-end items-center">
         <DropDownMobileMenu />
--- blogViewer/src/components/pages/layout.tsx
+++ blogViewer/src/components/pages/layout.tsx
@@ -4,8 +4,8 @@ import { Toaster } from "sonner";
 
 export default function Layout() {
   return (
-    <div className="w-screen flex justify-center items-start bg-darkprimary">
-      <div className="p-4 md:p-6 lg:p-8 mainBody flex flex-col content-start w-screen max-w-5xl min-h-screen overflow-auto bg-darkprimary shadow-md ">
+    <div className="layoutWideContainer w-screen flex justify-center items-start bg-darkprimary overflow-clip">
+      <div className="layoutMain p-4 md:p-6 lg:p-8 mainBody flex flex-col content-start w-screen max-w-5xl min-h-screen overflow-auto bg-darkprimary shadow-md mb-10">
         <HeaderBar />
         <main className="flex flex-col w-full h-[calc(100vh-112px)]">
           <Outlet />
